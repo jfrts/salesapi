3500f7b45181f295e96621c17d1af7902f356ad1
jcfrts <jcsr.frts@gmail.com>
2022-05-23 16:08:24 -0300

    created: user typeorm repository

---
+++ src/modules/users/typeorm/repositories/user.repository.ts
@@ -0,0 +1,25 @@
+import { AppDataSource } from '@/shared/typeorm';
+import { User } from '../entities/User';
+
+export const ProductRepository = AppDataSource.getRepository(User).extend({
+    async findByName(name: string): Promise<User | null> {
+        const user = this.findOne({
+            where: { name },
+        });
+        return user;
+    },
+
+    async findById(id: string): Promise<User | null> {
+        const user = this.findOne({
+            where: { id },
+        });
+        return user;
+    },
+
+    async findByEmail(email: string): Promise<User | null> {
+        const user = this.findOne({
+            where: { email },
+        });
+        return user;
+    },
+});
